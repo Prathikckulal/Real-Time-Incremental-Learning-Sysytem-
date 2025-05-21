997bdfa2f0d6c9f9ba02e1f63b15434c49484c12
Prathik-c-kulal <62025401+Prathikckulal@users.noreply.github.com>
2025-05-21 16:25:02 +0530

    Update train_model.py

--- tenserflow_code/train_model.py
+++ tenserflow_code/train_model.py
@@ -1,14 +1,7 @@
 import tensorflow as tf
- 
-# Load preprocessed data
 
 dataset = tf.data.Dataset.load('preprocessed_data_path')
- 
-# Define the model
-
 model = tf.keras.Sequential([...])
- 
-# Train the model incrementally
 
 for batch in dataset:
 
